1ea5a3d2b030de12228ab2a3c1b6008d5120f4d0
Ujjawal Kumar <ujjawalrr@gmail.com>
2023-08-15 18:08:15 +0530

    second  commit

--- server.js
+++ server.js
@@ -18,17 +18,44 @@ app.get('/login', (req, res) => {
 app.get('/register', (req, res) => {
     res.render('register');
 })
-app.get('/contact', (req, res) => {
-    res.render('contact');
+app.get('/forgotPassword', (req, res) => {
+    res.render('forgotPassword');
 })
-app.get('/privacy', (req, res) => {
+app.get('/resetPassword', (req, res) => {
+    res.render('resetPassword');
+})
+app.get('/emailVerified', (req, res) => {
+    res.render('emailVerified');
+})
+app.get('/emailUnverified', (req, res) => {
+    res.render('emailUnverified');
+})
+app.get('/profile', (req, res) => {
+    res.render('profile');
+})
+app.get('/updateProfile', (req, res) => {
+    res.render('updateProfile');
+})
+app.get('/privacyPolicy', (req, res) => {
     res.render('privacyPolicy');
 })
 app.get('/terms', (req, res) => {
     res.render('terms');
 })
-app.get('/carousel', (req, res) => {
-    res.render('carousel');
+app.get('/contact', (req, res) => {
+    res.render('contact');
+})
+app.get('/confirmation20', (req, res) => {
+    res.render('confirmation20');
+})
+app.get('/confirmationCall', (req, res) => {
+    res.render('confirmationCall');
+})
+app.get('/courseDescription', (req, res) => {
+    res.render('courseDescription');
+})
+app.get('/courseProgress', (req, res) => {
+    res.render('courseProgress');
 })
 
 app.listen(port, () => {
